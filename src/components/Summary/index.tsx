7ab307b2006f9a1d61f667b706f6a6846349fc91
Wallyson Jhonatan de Oliveira <waloli@futura.sbo>
2024-02-05 16:27:04 -0300

    test

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,7 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { SummaryCard, SummaryContainer } from "./styles";
-import { priceFormatter } from "../../Utils/formatter";
 import { useSummary } from "../../hooks/useSummary";
+import { priceFormatter } from "../../Utils/formatter";
+import { SummaryCard, SummaryContainer } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 
 export function Summary() {
   const summary = useSummary(); 
